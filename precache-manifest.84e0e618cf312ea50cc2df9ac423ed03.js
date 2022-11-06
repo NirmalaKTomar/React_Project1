cb8133ee6ab9eead586899c29dfbb7fed9670b08
Code Sandbox <hello@codesandbox.io>
2022-11-06 15:19:20 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.84e0e618cf312ea50cc2df9ac423ed03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0a3b5d9aa244b84853181591de37622",
+    "url": "https://nirmalaktomar.github.io/csb-8z4gf6/index.html"
+  },
+  {
+    "revision": "b2791fbf57c46bb11e40",
+    "url": "https://nirmalaktomar.github.io/csb-8z4gf6/static/css/main.0df9532a.chunk.css"
+  },
+  {
+    "revision": "9ef99623ff84a3fe9477",
+    "url": "https://nirmalaktomar.github.io/csb-8z4gf6/static/js/2.ac40023b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "https://nirmalaktomar.github.io/csb-8z4gf6/static/js/2.ac40023b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2791fbf57c46bb11e40",
+    "url": "https://nirmalaktomar.github.io/csb-8z4gf6/static/js/main.5ec6fdec.chunk.js"
+  },
+  {
+    "revision": "118b648ab0e022e1ab3c",
+    "url": "https://nirmalaktomar.github.io/csb-8z4gf6/static/js/runtime-main.3d5fc2ed.js"
+  }
+]);
